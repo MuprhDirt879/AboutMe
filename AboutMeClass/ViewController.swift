73152c5a62f1ce5e4878db00d14d704d8a83f503
Ryan Murphy <thomasryanmurphy@gmail.com>
2019-04-23 12:30:22 -0400

    Initial Commit

---
+++ AboutMeClass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMeClass
+//
+//  Created by Ryan Murphy on 4/23/19.
+//  Copyright © 2019 Ryan Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
